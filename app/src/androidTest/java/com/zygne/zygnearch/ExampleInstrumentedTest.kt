26f6158bb5b96d65d272411220f543d6eb9ce0f0
bardur <zygneapps@gmail.com>
2021-10-04 16:09:37 -0500

    update tests to kt

--- app/src/androidTest/java/com/zygne/zygnearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zygne/zygnearch/ExampleInstrumentedTest.kt
@@ -1,26 +1,22 @@
-package com.zygne.zygnearch;
+package com.zygne.zygnearch
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.zygne.zygnearch", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getTargetContext()
+        Assert.assertEquals("com.zygne.zygnearch", appContext.packageName)
     }
 }
